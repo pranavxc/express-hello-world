3e15953566b851274cf89627e606db42a3a4ebf6
Kam <seekayel@users.noreply.github.com>
2021-09-15 07:19:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static('public', options))
 // Catch all handler for all other request.
 app.use('*', (req,res) => {
   res.json({
-      msg: 'Great Day',
+      msg: 'This magic moment',
       at: new Date().toISOString(),
       method: req.method,
       hostname: req.hostname,
