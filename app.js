4e4d37e5550c98341c5a676610bac73a3cdb3203
Kam <seekayel@users.noreply.github.com>
2021-09-15 08:04:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static('public', options))
 // Catch all handler for all other request.
 app.use('*', (req,res) => {
   res.json({
-      msg: 'This magic moment...',
+      msg: 'Multi-region',
       at: new Date().toISOString(),
       method: req.method,
       hostname: req.hostname,
