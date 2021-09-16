718cb96600ae99d70085b801ce5cc28ff2720a93
Kam <seekayel@users.noreply.github.com>
2021-09-16 09:33:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use('*', (req,res) => {
   var region = (process.env.region)? process.env.region : 'undefined'
   console.error(region)
   res.json({
-      message: 'Demo Demo',
+      message: 'Loom Demo',
       region,
       at: new Date().toISOString(),
       method: req.method,
