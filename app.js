442e9ae743d5f396676c8d536ac0df2db5b81cd5
Kam <seekayel@users.noreply.github.com>
2021-09-24 07:16:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('*', (req,res) => {
   var region = (process.env.region)? process.env.region : 'undefined'
   console.error(region)
   res.json({
-      message: 'msg: Deployed to multiple regions',
+      message: 'msg: Deployed',
       region,
       at: new Date().toISOString(),
       params: req.params,
