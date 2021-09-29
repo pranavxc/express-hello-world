25ee03d0fc1f0f916c18c4e5a3fd9aa50706f9f8
Kam <seekayel@users.noreply.github.com>
2021-09-29 10:47:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/e/*', (req,res,next) => {
 app.use('*', (req,res) => {
   var region = (process.env.region)? process.env.region : 'undefined'
   res.json({
-      message: 'msg: start',
+      message: 'msg: deployed new version',
       region,
       path: req.originalUrl,
       at: new Date().toISOString(),
