7ce41e27eebdc267a788066b2d707b42b0c3cdf4
Kam <seekayel@users.noreply.github.com>
2021-10-25 14:08:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/e/*', (req,res,next) => {
 app.use('*', (req,res) => {
   var region = (process.env.region)? process.env.region : 'undefined'
   res.json({
-      message: 'msg: great day today',
+      message: 'msg: what a great day',
       region,
       path: req.originalUrl,
       at: new Date().toISOString(),
